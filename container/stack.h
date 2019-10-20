dbd8e4e67134685041a0a2c6bbd66d6e789f5a48
DunderBird <522169030@qq.com>
2019-10-20 13:05:03 +0800

    implement stack

---
+++ container/stack.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "vector.h"
+
+namespace DS {
+    template<typename T, typename Sequence = vector<T> >
+    class stack
+    {
+    public:
+        using value_type = typename Sequence::value_type;
+        using size_type = typename Sequence::size_type;
+        using difference_type = typename Sequence::difference_type;
+        using pointer = typename Sequence::pointer;
+        using reference = typename Sequence::reference;
+        using const_reference = typename Sequence::const_reference;
+        
+    private:
+        Sequence seq;
+        using self = stack<T, Sequence>;
+
+    public:
+        bool empty() const { return seq.empty(); }
+        size_type size() const { return seq.size(); }
+        const_reference top() const { return seq.back(); }
+        reference top() { return seq.back(); }
+        void push(const value_type &x) {
+            seq.push_back(x);
+        }
+        void pop() {
+            seq.pop_back();
+        }
+        
+        bool operator == (const self &x, const self &y) const {
+            return x.seq == y.seq;
+        }
+        bool operator < (const self &x, const self &y) const {
+            return x.seq < y.seq;
+        }
+    };
+}
