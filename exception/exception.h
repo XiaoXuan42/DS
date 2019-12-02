2ac28a9ef1df5e7756e32d870e2b02221a8695d5
DunderBird <522169030@qq.com>
2019-12-02 17:37:52 +0800

    add DSReadError

--- exception/exception.h
+++ exception/exception.h
@@ -15,4 +15,11 @@ namespace DS {
             return "You are trying to operate on an invalid memory address\n";
         }
     };
+    class DSReadError : public DSException
+    {
+    public:
+        virtual const char * what() {
+            return "The element you are trying to read is not found";
+        }
+    };
 }
